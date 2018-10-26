7af5c4c32c189b1ac428d7db11164bd18879f07b
Neetish Raj <neetishop@gmail.com>
2018-10-26 07:32:49 +0530

    Excluded config.js from .gitignore

---
+++ config.js
@@ -0,0 +1,84 @@
+/*
+ * Create and export configuration variables
+ */
+
+// Container for all the envronments
+const environments = {};
+
+// environment configuration for testing: NODE_ENV="test"
+environments.test = {
+    "envName": "test",
+    "hashingSecret": "ThisIsMyHashingSecret",
+    "httpPort": 8080,
+    "httpsPort": 3001,
+    "twitter": {
+        "consumerKey": 'p9iYqC8nyAd8ZBpqHQdgFopr0',
+        "consumerSecret": 'R0DZdQUMO9dZVTKYfWosX1luZXXcWIKIMi7QUWuHmOwFef0bAY'
+    },
+    "db": {
+        "name": "twitterfeed",
+        "username": "username",
+        "password": "Mongodb500"
+    }
+
+};
+
+// Staging (default) environment: NODE_ENV="staging"
+environments.staging = {
+    "envName": "staging",
+    "hashingSecret": "ThisIsMyHashingSecret",
+    "httpPort": 8080,
+    "httpsPort": 3001,
+    "twitter": {
+        "consumerKey": 'p9iYqC8nyAd8ZBpqHQdgFopr0',
+        "consumerSecret": 'R0DZdQUMO9dZVTKYfWosX1luZXXcWIKIMi7QUWuHmOwFef0bAY'
+    },
+    "db": {
+        "name": "twitterfeed",
+        "username": "username",
+        "password": "Mongodb500"
+    }
+
+};
+
+// Production environment: NODE_ENV="production"
+environments.production = {
+    "envName": "production",
+    "hashingSecret": "ThisIsMyHashingSecret",
+    "httpPort": process.env.PORT,
+    "httpsPort": 5001,
+    "twitter": {
+        "consumerKey": 'p9iYqC8nyAd8ZBpqHQdgFopr0',
+        "consumerSecret": 'R0DZdQUMO9dZVTKYfWosX1luZXXcWIKIMi7QUWuHmOwFef0bAY'
+    },
+    "db": {
+        "name": "twitterfeed",
+        "username": "username",
+        "password": "Mongodb500"
+    }
+
+}
+
+/*
+ * Determine which environment is exported based on the
+ * NODE_ENV variable as passed from the command line during start of the app
+ */
+
+let currentEnvironment = "";
+
+if (typeof process.env.NODE_ENV === "string") {
+    currentEnvironment = process.env.NODE_ENV.toLowerCase();
+}
+
+/*
+ * Now look whether the given environment string is available or not
+ * If not then export the default staging environment object
+ */
+
+let environmentToExport = environments.staging;
+
+if (typeof environments[currentEnvironment] === "object") {
+    environmentToExport = environments[currentEnvironment];
+}
+
+module.exports = environmentToExport;
